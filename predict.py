89fbd314338b4b2eccce7fb1aecf7bf63d5c54a2
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2019-05-09 13:59:57 -0700

    Use mlflow.pyfunc.PythonModel

--- predict.py
+++ predict.py
@@ -2,6 +2,7 @@ import logging
 import pickle
 import sys
 
+import mlflow.pyfunc
 import numpy as np
 import pandas as pd
 import torch
@@ -22,12 +23,6 @@ init_logging(log_dir="logs")
 logger = logging.getLogger(__name__)
 
 PARALLEL = 4 # assuming a quad-core machine
-ATTRIBUTE = "pob_presidents"
-conn_string = 'postgresql://localhost:5432/' + ATTRIBUTE
-session = Meta.init(conn_string).Session()
-
-from fonduerconfig import matchers, mention_classes, mention_spaces, candidate_classes  # isort:skip
-
 
 def get_entity_relation(candidate):
     return tuple(([m.context.get_span() for m in candidate.get_mentions()]))
@@ -41,58 +36,66 @@ def get_unique_entity_relations(candidates):
     return unique_entity_relation
 
 
-def predict(model_input):
-    for index, row in model_input.iterrows():
-        _process(row['filename'])
+class FonduerModel(mlflow.pyfunc.PythonModel):
 
-def _process(filename):
-    # Parse docs
-    logger.info("parsing...")
-    docs_path = filename
-    doc_preprocessor = HTMLDocPreprocessor(docs_path)
-    corpus_parser = Parser(session, structural=True, lingual=True)
-    # clear=False otherwise gets stuck.
-    corpus_parser.apply(doc_preprocessor, clear=False, parallelism=PARALLEL)
-    test_docs = corpus_parser.get_last_documents()
+    def predict(self, context, model_input):
+        for index, row in model_input.iterrows():
+            self._process(row['filename'])
 
-    mention_extractor = MentionExtractor(
-        session,
-        mention_classes, mention_spaces, matchers
-    )
-    mention_extractor.apply(test_docs, clear=False, parallelism=PARALLEL)
+    def _process(self, filename):
+        # Parse docs
+        logger.info("parsing...")
+        docs_path = filename
+        doc_preprocessor = HTMLDocPreprocessor(docs_path)
+        corpus_parser = Parser(session, structural=True, lingual=True)
+        # clear=False otherwise gets stuck.
+        corpus_parser.apply(doc_preprocessor, clear=False, parallelism=PARALLEL)
+        test_docs = corpus_parser.get_last_documents()
 
-    # Candidate
-    candidate_extractor = CandidateExtractor(session, candidate_classes)
-    candidate_extractor.apply(test_docs, split=2, clear=True, parallelism=PARALLEL)
-    test_cands = candidate_extractor.get_candidates(split=2)
+        mention_extractor = MentionExtractor(
+            session,
+            mention_classes, mention_spaces, matchers
+        )
+        mention_extractor.apply(test_docs, clear=False, parallelism=PARALLEL)
 
-    # Featurization
-    featurizer = Featurizer(session, candidate_classes)
-    with open('feature_keys.pkl', 'rb') as f:
-        key_names = pickle.load(f)
-    featurizer.drop_keys(key_names)
-    featurizer.upsert_keys(key_names)
-    featurizer.apply(test_docs, clear=False)
-    F_test = featurizer.get_feature_matrices(test_cands)
+        # Candidate
+        candidate_extractor = CandidateExtractor(session, candidate_classes)
+        candidate_extractor.apply(test_docs, split=2, clear=True, parallelism=PARALLEL)
+        test_cands = candidate_extractor.get_candidates(split=2)
 
-    disc_model = LogisticRegression()
+        # Featurization
+        featurizer = Featurizer(session, candidate_classes)
+        with open('feature_keys.pkl', 'rb') as f:
+            key_names = pickle.load(f)
+        featurizer.drop_keys(key_names)
+        featurizer.upsert_keys(key_names)
+        featurizer.apply(test_docs, clear=False)
+        F_test = featurizer.get_feature_matrices(test_cands)
 
-    # Workaround to https://github.com/HazyResearch/fonduer/issues/208
-    checkpoint = torch.load("./best_model.pt")
-    disc_model.settings = checkpoint["config"]
-    disc_model.cardinality = checkpoint["cardinality"]
-    disc_model._build_model()
+        disc_model = LogisticRegression()
 
-    disc_model.load(model_file="best_model.pt", save_dir="./")
+        # Workaround to https://github.com/HazyResearch/fonduer/issues/208
+        checkpoint = torch.load("./best_model.pt")
+        disc_model.settings = checkpoint["config"]
+        disc_model.cardinality = checkpoint["cardinality"]
+        disc_model._build_model()
 
-    test_score = disc_model.predict((test_cands[0], F_test[0]), b=0.6, pos_label=TRUE)
-    true_preds = [test_cands[0][_] for _ in np.nditer(np.where(test_score == TRUE))]
+        disc_model.load(model_file="best_model.pt", save_dir="./")
 
-    for entity_relation in get_unique_entity_relations(true_preds):
-        print(entity_relation)
+        test_score = disc_model.predict((test_cands[0], F_test[0]), b=0.6, pos_label=TRUE)
+        true_preds = [test_cands[0][_] for _ in np.nditer(np.where(test_score == TRUE))]
+
+        for entity_relation in get_unique_entity_relations(true_preds):
+            print(entity_relation)
 
 
 if __name__ == '__main__':
+    ATTRIBUTE = "pob_presidents"
+    conn_string = 'postgresql://localhost:5432/' + ATTRIBUTE
+    session = Meta.init(conn_string).Session()
+    from fonduerconfig import matchers, mention_classes, mention_spaces, candidate_classes  # isort:skip
+    model = FonduerModel()
+
     filename = sys.argv[1]
     model_input = pd.DataFrame({'filename': [filename]})
-    predict(model_input)
+    model.predict(None, model_input)
